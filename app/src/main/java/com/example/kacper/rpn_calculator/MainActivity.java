f5cf8dc658a4e95511b49c2b68101c5bb3fb8b8e
kakapela <kacperkapela1@gmail.com>
2018-04-17 22:12:05 +0200

    Event Handlers and Callback Methods.

--- app/src/main/java/com/example/kacper/rpn_calculator/MainActivity.java
+++ app/src/main/java/com/example/kacper/rpn_calculator/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,145 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView maininput = (TextView)findViewById(R.id.mainInput);
+        Button button0 = (Button)findViewById(R.id.button0);
+        Button button1 = (Button)findViewById(R.id.button1);
+        Button button2 = (Button)findViewById(R.id.button2);
+        Button button3 = (Button)findViewById(R.id.button3);
+        Button button4 = (Button)findViewById(R.id.button4);
+        Button button5 = (Button)findViewById(R.id.button5);
+        Button button6 = (Button)findViewById(R.id.button6);
+        Button button7 = (Button)findViewById(R.id.button7);
+        Button button8 = (Button)findViewById(R.id.button8);
+        Button button9 = (Button)findViewById(R.id.button9);
+        Button acButton = findViewById(R.id.ACButton);
+
+        //event handler and inside him callback method
+        button0.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        //append zero only if mainINPUT is not empty -> we can not type 0 at the beginnig
+                        //append zero until limit 10 numbers -> because UI can crashed
+                        if(!(maininput.getText().toString().matches("")) && inputCounter<10)
+                        maininput.append("0");
+                    }
+                }
+        );
+
+        button1.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+
+                        if(inputCounter<10)
+                        maininput.append("1");
+                    }
+                }
+        );
+        button2.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("2");
+                    }
+                }
+        );
+        button3.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("3");
+                    }
+                }
+        );
+        button4.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("4");
+                    }
+                }
+        );
+        button5.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("5");
+                    }
+                }
+        );
+        button6.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("6");
+                    }
+                }
+        );
+        button6.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("6");
+                    }
+                }
+        );
+
+        button7.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("7");
+                    }
+                }
+        );
+        button8.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("8");
+                    }
+                }
+        );
+        button9.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                        maininput.append("9");
+                    }
+                }
+        );
+        acButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        int inputCounter = maininput.getText().length();
+                        if(inputCounter<10)
+                            maininput.setText("0");
+                    }
+                }
+        );
     }
 
     @Override
