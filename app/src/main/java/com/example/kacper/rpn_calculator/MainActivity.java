a88581519c88860d4f1089c6c8ca664c6e57e330
kakapela <kacperkapela1@gmail.com>
2018-04-21 22:04:07 +0200

    Creating landscapes for every activities.

--- app/src/main/java/com/example/kacper/rpn_calculator/MainActivity.java
+++ app/src/main/java/com/example/kacper/rpn_calculator/MainActivity.java
@@ -856,4 +856,6 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/example/kacper/rpn_calculator/Operations.java
@@ -0,0 +1,12 @@
+package com.example.kacper.rpn_calculator;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class Operations {
+
+}
